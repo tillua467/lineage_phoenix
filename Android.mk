0bd1a65652677bd3ffa9437c6b553d717a3d9572
Tillua467 <128046674+tillua467@users.noreply.github.com>
2024-12-04 17:08:48 +0600

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -8,22 +8,21 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),phoenix)
 
-
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 # Add module definition for libhvx_interface
-
 include $(CLEAR_VARS)
 LOCAL_MODULE := libhvx_interface
-LOCAL_SRC_FILES := proprietary/vendor/lib64/libhvx_interface.so
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(TARGET_OUT)/system/lib64
-LOCAL_SHARED_LIBRARIES := liblog libcdsprpc libm libc
-DISABLE_ELFCHECK := true
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MULTILIB := 64
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
+LOCAL_SRC_FILES := vendor/lib64/libhvx_interface.so
+LOCAL_CHECK_ELF_FILES := false  # This is the correct way to disable ELF checks
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libcdsprpc
 
-include $(BUILD_SHARED_LIBRARY)
-
-endif
-
+include $(BUILD_PREBUILT)
 
 endif
