b391ed69d238e266d8bd04abba59cda4dba5032d
Tillua467 <128046674+tillua467@users.noreply.github.com>
2024-12-04 16:28:41 +0600

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -10,4 +10,23 @@ ifeq ($(TARGET_DEVICE),phoenix)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+ifeq ($(TARGET_DEVICE),phoenix)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+# Add module definition for libhvx_interface
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libhvx_interface
+LOCAL_SRC_FILES := proprietary/vendor/lib64/libhvx_interface.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT)/system/lib64
+LOCAL_SHARED_LIBRARIES := liblog libcdsprpc libm libc
+DISABLE_ELFCHECK := true
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif
+
+
 endif
