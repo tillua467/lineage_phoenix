54bdf365db6ed4aeafd507ac9f218bf41cef71eb
Tillua467 <128046674+tillua467@users.noreply.github.com>
2024-12-09 12:03:28 +0600

    Update lineage_phoenix.mk

--- lineage_phoenix.mk
+++ lineage_phoenix.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,35 +11,37 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from phoenix device
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# GMS
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+# Inherit some common Evolution stuff.
+TARGET_DISABLE_EPPE := true
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+EVO_BUILD_TYPE := Unofficial
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_PHONE_LINK_SUPPORTED := true
+TARGET_USES_PICO_GAPPS := true
 
 # Custom stuff.
-TARGET_BUILD_APERTURE_CAMERA := false
+TARGET_BUILD_APERTURE_CAMERA := true
 TARGET_USE_PIXEL_FINGERPRINT := true
 TARGET_INCLUDE_PIXEL_CHARGER := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
-# Evolution X Build Type & Maintainer stuff.
-EVO_BUILD_TYPE := Unofficial
-
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-TARGET_BOOT_ANIMATION_RES := 1080
-
-
-# Build Fingerprint.
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="phoenix-user 11 RKQ1.200826.002 21.9.28 release-keys" \
-    BuildFingerprint=Redmi/phoenix/phoenix:11/RKQ1.200826.002/21.9.28:user/release-keys
-
-
+# Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30
 PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
+BOARD_VENDOR := Xiaomi
 
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+TARGET_VENDOR_PRODUCT_NAME := phoenix
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="phoenix-user 11 RKQ1.200826.002 21.9.28 release-keys"
+
+Build_Fingerprint := Redmi/phoenix/phoenix:11/RKQ1.200826.002/21.9.28:user/release-keys
